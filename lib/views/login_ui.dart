d295873a0cdf59966b196e85bad5e05093290382
srssunter <sirisak.saechia23579@gmail.com>
2026-02-06 20:43:06 +0700

    Complete V.1.0.0

--- lib/views/login_ui.dart
+++ lib/views/login_ui.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginUi extends StatefulWidget {
   const LoginUi({super.key});
@@ -14,53 +17,204 @@ class _LoginUiState extends State<LoginUi> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Align(
-                alignment: AlignmentGeometry.centerLeft,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Icon(
-                    Icons.arrow_back_ios_rounded,
-                    size: 45.0,
-                    color: Colors.grey[800],
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Center(
+            child: Column(
+              children: [
+                Align(
+                  alignment: AlignmentGeometry.centerLeft,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.arrow_back_ios_rounded,
+                      size: 45.0,
+                      color: Colors.grey[800],
+                    ),
                   ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  width: 250.0,
-                  height: 250.0,
-                  fit: BoxFit.cover,
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 220.0,
+                    height: 220.0,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Welecome Back!',
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Welecome Back!',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Make it work,make it right,make it fast.',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.person_2_outlined,
+                      color: Colors.grey[400],
+                    ),
+                    hintText: 'E-Mail',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 22.0,
+                      horizontal: 15.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.fingerprint,
+                      color: Colors.grey[500],
+                    ),
+                    suffixIcon: Icon(
+                      Icons.visibility,
+                      color: Colors.grey[500],
+                    ),
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 22.0,
+                      horizontal: 15.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Forget Password?',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      55.0,
+                    ),
+                    backgroundColor: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  'OR',
                   style: TextStyle(
-                    fontSize: 35.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey[800],
+                    fontSize: 18.0,
                   ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Make it work,make it right,make it fast.',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.grey[800],                
+                SizedBox(height: 20.0),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/googlelogo.png',
+                        width: 25,
+                        height: 25,
+                        fit: BoxFit.cover,
+                      ),
+                       SizedBox(
+                    width: 10.0,
+                  ),
+                      Text(
+                        'Sign-in with Google',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      55.0,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                 SizedBox(
+                    height: 20.0,
+                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Signup',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/views/signup_ui.dart
+++ lib/views/signup_ui.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignupUi extends StatefulWidget {
   const SignupUi({super.key});
@@ -11,7 +14,229 @@ class _SignupUiState extends State<SignupUi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Center(
+            child: Column(
+              children: [
+                Align(
+                  alignment: AlignmentGeometry.centerLeft,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.arrow_back_ios_rounded,
+                      size: 45.0,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 220.0,
+                    height: 220.0,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Get on Board!',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Create your profile to start your Journey.',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.person_2_outlined,
+                      color: Colors.grey[400],
+                    ),
+                    hintText: 'Full Name',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 22.0,
+                      horizontal: 15.0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.grey[400],
+                    ),
+                    hintText: 'E-Mail',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 22.0,
+                      horizontal: 15.0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.hashtag,
+                      color: Colors.grey[400],
+                    ),
+                    hintText: 'Phone No',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 22.0,
+                      horizontal: 15.0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.fingerprint,
+                      color: Colors.grey[500],
+                    ),
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 22.0,
+                      horizontal: 15.0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SINGUP',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      55.0,
+                    ),
+                    backgroundColor: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'OR',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/googlelogo.png',
+                        width: 25,
+                        height: 25,
+                        fit: BoxFit.cover,
+                      ),
+                       SizedBox(
+                    width: 10.0,
+                  ),
+                      Text(
+                        'SIGN-IN WITH GOOGLE',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      55.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                    height: 20.0,
+                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have on Account?',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
